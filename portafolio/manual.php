eee09cbeb0df7d3ea9438a57de63c8c0894835cb
tativargas04 <ptvargas3@misena.edu.co>
2021-10-05 18:30:28 -0500

    cambios2

--- portafolio/manual.php
+++ portafolio/manual.php
@@ -7,6 +7,14 @@
     <title>Document</title>
 </head>
 <body>
-    <p>manual usuario</p>
+    <<div class="menu">
+                <ul>
+                    <li><a class="active" href="http://localhost/TATIANA/portafolio/quiensoy.php">¿Quien soy?</a></li>
+                    <li><a href="http://localhost/TATIANA/portafolio/manual.php">Manual </a></li>
+                    <li><a href="http://localhost/TATIANA/portafolio/blogs.php">Blogs</a></li>
+                    <li><a href="http://localhost/TATIANA/portafolio/crud.php">Crud</a></li>
+                    <li><a href="http://localhost/TATIANA/portafolio/java.js">Java</a></li>
+                </ul>
+            </div>
 </body>
 </html>
