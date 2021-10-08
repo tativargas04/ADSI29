83952f8d5287d9d08877ef8c863ed33430bedcd2
tativargas04 <ptvargas3@misena.edu.co>
2021-10-07 21:47:35 -0500

    fraccionarios

--- adsi2142329/6.clase 01-10-2021/mision/tareaejercicios.js
+++ adsi2142329/6.clase 01-10-2021/mision/tareaejercicios.js
@@ -1,11 +1,40 @@
-/*var fraccionario{
-    n1:1,
-    n2:2,
-    d1:2,
-    d2:3,
-    suma:function() {
-        return this.n1
-        
-    }
+//actividad fracciones 
 
-}*/
+var frac={
+    n1:1,
+    n2:4,
+    d1:7,
+    d2:3,
+    suma:function(){
+        numeradorS1=this.n1 * this.d2;
+        numeradorS2=this.n2 * this.d1;
+        numeradorSF=numeradorS1 + numeradorS2;
+        denominadorS=this.d1 * this.d2;
+        return 'el resultado de la suma es '+ numeradorSF + ' / ' + denominadorS;
+    },
+    resta:function(){
+        numeradorR1=this.n1 * this.d2;
+        numeradorR2=this.n2 * this.d1;
+        numeradorRF=numeradorS1 - numeradorS2;
+        denominadorR=this.d1 * this.d2;
+        return 'el resultado de la resta es '+ numeradorRF + ' / ' + denominadorR;
+    },
+    multi:function(){
+        numeradorM1=this.n1 * this.n2;
+        numeradorM2=this.d1 * this.d2;
+        return 'el resultado de la multiplicacion es '+ numeradorM1 + ' / ' +numeradorM2;
+    },
+    divi:function(){
+        numeradorD1=this.n1 * this.d2;
+        numeradorD2=this.n2 * this.d1;
+        return 'el resultado de la division es '+ numeradorD1 + ' / ' +numeradorD2;
+    }
+}
+
+console.log(frac.suma());
+
+console.log(frac.resta());
+
+console.log(frac.multi());
+
+console.log(frac.divi());
